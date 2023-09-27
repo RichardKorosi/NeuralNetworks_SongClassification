095f9482ff6f86ac77c198b186e358413924ac74
Richard Körösi <korosi.richard@gmail.com>
2023-09-27 21:45:30 +0200

    Par zmien

--- main.py
+++ main.py
@@ -70,7 +70,11 @@ print("*" * 100, "Column types", "*" * 100)
 print(df.dtypes)
 
 df = pd.get_dummies(df, columns=['top_genre'], prefix='', prefix_sep='')  # Encode top_genre column
-df = pd.get_dummies(df, columns=['emotion'], prefix='', prefix_sep='')  # Encode emotion column
+# df = pd.get_dummies(df, columns=['emotion'], prefix='', prefix_sep='')  # Encode emotion column
+
+# Use Label encoding for country
+le = LabelEncoder()
+df['emotion'] = le.fit_transform(df['emotion'])
 
 print("*" * 100, "Column types", "*" * 100)
 print(df.dtypes)
@@ -80,13 +84,12 @@ df.to_csv('./data/zadanie1_dataset_clean.csv', index=False)
 # Split dataset into X and y (input and output) (0,5b) -----------------------------------------------------------------
 
 # Split dataset into X and y
-X = df.drop(columns=['calm', 'energetic', 'happy', 'sad'])
-y = df[['calm', 'energetic', 'happy', 'sad']]
+X = df.drop(columns=['emotion'])
+y = df['emotion']
 
 # Split dataset into train, valid and test
 X_train, X_valid_test, y_train, y_valid_test = train_test_split(X, y, shuffle=True, test_size=0.2, random_state=42)
-X_valid, X_test, y_valid, y_test = train_test_split(X_valid_test, y_valid_test, shuffle=True, test_size=0.5,
-                                                    random_state=42)
+X_valid, X_test, y_valid, y_test = train_test_split(X_valid_test, y_valid_test, shuffle=True, test_size=0.5, random_state=42)
 
 # Scale and standardize data (0,5b) ------------------------------------------------------------------------------------
 
@@ -144,12 +147,12 @@ print(X_train.max(numeric_only=True))
 
 # Train MLP model to predict country
 print("*"*100, "MLP", "*"*100)
-# print(f"Random accuracy: {1/len(y_train.unique())}")
+print(f"Random accuracy: {1/len(y_train.unique())}")
 
 clf = MLPClassifier(
     hidden_layer_sizes=(100, 100, 10, 10),
     random_state=1,
-    max_iter=10,
+    max_iter=100,
     validation_fraction=0.2,
     early_stopping=True,
     learning_rate='adaptive',
@@ -159,9 +162,32 @@ clf = MLPClassifier(
 # Predict on train set
 y_pred = clf.predict(X_train)
 print('MLP accuracy on train set: ', accuracy_score(y_train, y_pred))
-# cm_train = confusion_matrix(y_train, y_pred)
+cm_train = confusion_matrix(y_train, y_pred)
+
+# Predict on test set
+y_pred = clf.predict(X_test)
+print('MLP accuracy on test set: ', accuracy_score(y_test, y_pred))
+cm_test = confusion_matrix(y_test, y_pred)
+
+# Create class names for confusion matrix
+class_names = list(le.inverse_transform(clf.classes_))
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm_train, display_labels=class_names)
+fig, ax = plt.subplots(figsize=(10,10))
+disp.plot(ax=ax)
+disp.ax_.set_title("Confusion matrix on train set")
+disp.ax_.set(xlabel='Predicted', ylabel='True')
+plt.show()
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm_test, display_labels=class_names)
+fig, ax = plt.subplots(figsize=(10,10))
+disp.plot(ax=ax)
+disp.ax_.set_title("Confusion matrix on test set")
+disp.ax_.set(xlabel='Predicted', ylabel='True')
+plt.show()
 
 # Poznamky na opytanie
 # 1. Pri filtrovani outlinerov sa mi rovno podarilo odstranovat aj null hodnoty, ci to je ok?
-# 2. Po zakodovani neciselnych hodnot mam plno grafov. Je to ok?
-# y_train nefunguje?
+# 2. Vymazanie genres, filtered_genres okrem name a url, je to ok?
+# 3. Po zakodovani neciselnych hodnot mam plno grafov. Je to ok?
+# 4. emotion treba cez label encoding?
